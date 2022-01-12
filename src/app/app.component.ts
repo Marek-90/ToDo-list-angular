2c0cec61c5d7b27b037b4a64a2f51fbd2746fc90
Marek Sieńkowski <mareksienkowski@MacBook-Pro-Marek.local>
2022-01-12 12:47:34 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // jeszcze tutaj nic nie ma
+  title = 'Lista Zadań'
+
+  get footer(): string {
+    return '2022 © Lista zadań zbudowana w Angularze.'
+  }
 }
