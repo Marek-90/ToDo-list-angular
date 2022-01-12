4794b20b0e446c6ba4fa96185a4484e63363f833
Marek Sieńkowski <mareksienkowski@MacBook-Pro-Marek.local>
2022-01-12 15:35:12 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Lista Zadań'
-
-  get footer(): string {
-    return '2022 © Lista zadań zbudowana w Angularze.'
+  config: {[key: string]: string} | null = null;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-03',
+      done: false
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false
+    }
+  ]
+  
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista Zadań',
+        footer: '© Lista zadań zbudowana w Angularze.',
+        date: new Date().toDateString()
+      }
+    }, 500)
+    
   }
-}
+} 
